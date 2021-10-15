e5681b97ab8c5fc337492854b0a2886a7ff7479c
Morel Victor <morel.victor1@gmail.com>
2021-10-15 16:57:25 +0200

    Add first logic for monster card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
+import GifCard from "./Components/GifCard/GifCard";
 import NavBar from "./Components/NavBar/NavBar";
 
 function App() {
-  return (
-    <div className="App">
-      <NavBar />
-    </div>
-  );
+    return (
+        <div className="App">
+            <NavBar />
+            <GifCard />
+            <GifCard />
+        </div>
+    );
 }
 
 export default App;
